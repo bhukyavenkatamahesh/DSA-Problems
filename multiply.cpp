6f5e2f6b31be437cae33756d1735e4d7b3cb01a3
Mahesh <bhukyavenkatamahesh@gmail.com>
2026-01-16 11:28:17 +0530

    Multiply two numbers

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    int multiply = a * b;
+    cout << "Multiply: " << multiply << endl;
+    return 0;
+}
